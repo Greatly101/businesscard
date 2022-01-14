83eede6e552a20a7bb485cb24f366a984891b17d
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-14 15:23:42 +0000

    Set debugShowCheckedModebanner to true

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class _BusinessCardState extends State<BusinessCard> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: Scaffold(
         //backgroundColor: Colors.teal,
         body: SafeArea(
